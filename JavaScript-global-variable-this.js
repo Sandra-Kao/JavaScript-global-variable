3ddc97b47024011c41d4e30a247a75c49c13c116
Sandra Kao <63223781+Sandra-Kao@users.noreply.github.com>
2021-04-14 20:02:39 +0800

    Update JavaScript-global-variable-this.js

--- JavaScript-global-variable-this.js
+++ JavaScript-global-variable-this.js
@@ -1,10 +1,11 @@
 function addTwo(){  
-  console.log(this); //1.window
-  console.log(": /'this/' in function.");
-  throw('this is a empty function.');
+  console.log(this); //2. window
+  console.log(": 'this' in addTwo function is equal to global this");
+  throw('Throw this is a empty function.');
   //2.Uncaught this is a empty function.
 }
-  console.log(this); //window
-  console.log(": /'This/' in global.");
+
+console.log(this); //1. window
+console.log(": 'this' in global.");
 
 addTwo();
